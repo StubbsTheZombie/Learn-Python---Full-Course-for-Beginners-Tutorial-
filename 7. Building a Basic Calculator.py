dd9b44991175c596e6f4939549f0f1f78f53dff1
StubbsTheZombie <87995423+StubbsTheZombie@users.noreply.github.com>
2021-08-17 20:09:06 -0300

    Add files via upload

---
+++ 7. Building a Basic Calculator.py
@@ -0,0 +1,25 @@
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+result = num1 + num2
+
+print(result)
+#python always makes input into strings as default
+
+#int only whole numbers
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+result = int(num1) + int(num2)
+
+print(result)
+
+#float allows for any kind of number
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+result = float(num1) + float(num2)
+
+print(result)
---
+++ 9. Lists.py
@@ -0,0 +1,4 @@
+
+friends = ["Kevin", "Karen", "Jim"]
+
+print(friends)
