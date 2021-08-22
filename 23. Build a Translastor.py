33c57a745b9657cb5ee7160daf175d833df347ac
StubbsTheZombie <87995423+StubbsTheZombie@users.noreply.github.com>
2021-08-22 12:58:39 -0300

    Add files via upload

---
+++ 23. Build a Translastor.py
@@ -0,0 +1,33 @@
+'''
+Giraffe Language
+vowels -> g
+------------
+
+dog -> dgg
+cat -> cft
+'''
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
---
+++ 24. Comments.py
@@ -0,0 +1,18 @@
+
+'''
+isahidhuiahd
+usdauihahsdi
+uashduihaiujhdjia
+'''
+
+#This prints out a string
+
+print("Comments are fun")
+
+#print("Comments are fun")
+
+'''
+print("Comments are fun")
+'''
+
+#This program is cool
